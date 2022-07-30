94a9f9a377dddf8b8b030d4c1f715b104049374d
Anepan <110299068+Anepan@users.noreply.github.com>
2022-07-30 21:36:17 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,12 +21,12 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94763427338']
-global.premium = ['94763427338']
-global.ownernomer = '94763427338'
+global.owner = ['94760601107']
+global.premium = ['94760601107']
+global.ownernomer = '94760601107'
 global.ownername = '៚ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ͢⁸⁵⁴¹⃟✦⃝̮̮〽️'
-global.botname = 'VIHANGA MD BOT'
-global.footer = 'VIHANGA MD'
+global.botname = 'pathum MD BOT'
+global.footer = 'pathum MD'
 global.cap = '*Genarated By Vihanga Md*'
 global.ig = 'https://github.com/vihangabot'
 global.region = 'Sri Lanka, Sabaragamuwa, Embilipitiya'
